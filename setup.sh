e9415a29fd73f8fb94b7e51f274e30366dbc729b
kaodeuay <74397196+kaodeuay@users.noreply.github.com>
2021-05-02 03:02:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/.streamlit/
+mkdir -p %userprofile%/.streamlit/
 
 echo "\
 [server]\n\
@@ -6,4 +6,4 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-" > ~/.streamlit/config.toml
+" > %userprofile%/.streamlit/config.toml
